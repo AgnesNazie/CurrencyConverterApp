16f7353bc7b36807cfbbbfd8461b2fa29c08e071
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-13 10:44:08 +0100

    created a method to get users choice and display menu

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -2,6 +2,8 @@ package com.agnes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("\n=================================\n");
+        System.out.println("\nWELCOME TO CURRENCY CONVERTER APP\n");
+        System.out.println("\n=================================\n");
     }
 }
